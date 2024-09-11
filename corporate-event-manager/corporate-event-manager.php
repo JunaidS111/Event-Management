0f74a8d4775da83e9356bf0a0e2347734557c169
Junaid Shaikh <shekhjuned111@gmail.com>
2024-09-11 18:29:06 +0530

    Update corporate-event-manager.php

--- corporate-event-manager/corporate-event-manager.php
+++ corporate-event-manager/corporate-event-manager.php
@@ -13,6 +13,10 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
+if(!defined('ABSPATH')){
+    //exit;
+}
+
 // Load plugin files
 require_once plugin_dir_path(__FILE__) . 'includes/event-cpt.php';
 require_once plugin_dir_path(__FILE__) . 'includes/event-functions.php';

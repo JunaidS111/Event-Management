65d671416c0687cadebea90db7bb808c758625a5
JunaidS111 <MahamadJunedS@ipc.local>
2024-09-23 19:10:54 +0530

    Added Custom Post type

---
+++ corporate-event-manager/event-cpt.php
@@ -0,0 +1,25 @@
+<?php
+// Register Custom Post Type for Events
+function cem_register_event_cpt() {
+    $labels = array(
+        'name'                  => _x('Events', 'Post Type General Name', 'corporate-event-manager'),
+        'singular_name'         => _x('Event', 'Post Type Singular Name', 'corporate-event-manager'),
+        'menu_name'             => __('Events', 'corporate-event-manager'),
+        'all_items'             => __('All Events', 'corporate-event-manager'),
+        'add_new_item'          => __('Add New Event', 'corporate-event-manager'),
+        'edit_item'             => __('Edit Event', 'corporate-event-manager'),
+        'new_item'              => __('New Event', 'corporate-event-manager'),
+    );
+
+    $args = array(
+        'label'                 => __('Event', 'corporate-event-manager'),
+        'labels'                => $labels,
+        'supports'              => array('title', 'editor', 'thumbnail', 'excerpt'),
+        'public'                => true,
+        'has_archive'           => true,
+        'rewrite'               => array('slug' => 'events'),
+        'show_in_rest'          => true,
+    );
+    register_post_type('cem_event', $args);
+}
+add_action('init', 'cem_register_event_cpt');
